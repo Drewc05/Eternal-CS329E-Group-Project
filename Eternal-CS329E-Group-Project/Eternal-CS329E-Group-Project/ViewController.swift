122d94281d5d0828e98da4e6b1f42a97f82733ac
Andrew Conner <drewconner105@gmail.com>
2025-10-20 12:20:12 -0500

    Initial Xcode setup

---
+++ Eternal-CS329E-Group-Project/Eternal-CS329E-Group-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eternal-CS329E-Group-Project
+//
+//  Created by Andrew Conner on 10/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
